a33cff06956472d34f6924b3052367f9f3b05aef
Kazuma.I <51313744+Kazuma7@users.noreply.github.com>
2021-06-17 14:40:53 +0900

    normalize関数を定義

--- tp-grating.py
+++ tp-grating.py
@@ -9,31 +9,62 @@ width = 512
 height = 512
 
 a = 1.0
-n = 50
 
-def binary_g(array):
-    #周期回数と振幅「デフォルトより優先される」
+#sin波の配列を作る関数
+def sin_g(w, h):
     n = 5
-    a = 1.0
+    x = np.linspace(0, 2*np.pi*n, w)
+    nor_vec = np.ones((h))
+    x_outer = np.outer(nor_vec,x)
+    y = (a*np.pi) * np.sin(x_outer) + (a*np.pi)
     
-    x = np.linspace(0, 2*np.pi*n, width)
-    y = (a*np.pi) * np.sin(x) + (a*np.pi)
-    y_binary = np.where(y <= a*np.pi, 0, a*np.pi)
+    #sin波をplotしたいとき
+    #plot_y = (a*np.pi) * np.sin(x) + (a*np.pi)
+    # plt.plot(x,plot_y)
+    # plt.show()
     
-    plt.plot(y_binary)
-    plt.show()
+    return y
+
+#与えられた配列を２値表現に変更する
+def binary_g(array, b_min, b_max, b_split):
+    #２値に分別
+    y_binary = np.where(array <= b_split, b_min, b_max)
+    
+    #画像を表示
+    # plt.plot(y_binary)
+    # plt.show()
     
     return y_binary
+
+#目的の最大値と最小値の配列に変換してくれる関数
+def normalize(array,  to_min, to_max):
+    array_min = np.min(array)
+    array_max = np.max(array)
+    to_array = (array - array_min) / (array_max - array_min) * (to_max - to_min) + to_min
+    return to_array
+    
+#画像として保存
+def bmp_store(img):
+    #nor8 = np.uint8(255*(img/(2*np.pi)))
+    nor8 = normalize(img, 0, 255)
+    
+    pil_img = Image.fromarray(nor8)
+    pil_img.show()
+    pil_img.save("grating_phase.bmp")
 
 if __name__ == '__main__':
     
     #1. 2次元のグレーティングを設計する
-    g_phase = np.zeros((width, height), dtype = float)
-    binary_g = binary_g(g_phase)
+    g_phase = sin_g(width,height)
+    
+    #2.2値表現に変更する
+    binary_g = binary_g(g_phase,0,a*np.pi,a*np.pi)
     
     #2. 設計したグレーティングを画像として表示して保存
+    bmp_store(binary_g)
     
     #3. グレーティングに照射するレーザービーム面を設計
+    
     
     #4 レーザービームを画像として表示して保存
     
