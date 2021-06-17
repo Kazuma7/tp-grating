72212b99b057e5c9c73777b9717a327ed9ddbba2
Kazuma.I <51313744+Kazuma7@users.noreply.github.com>
2021-06-17 13:40:54 +0900

    ２値変換の処理を追加

--- tp-grating.py
+++ tp-grating.py
@@ -4,9 +4,32 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import time
 
+#パラメータの設定
+width = 512
+height = 512
+
+a = 1.0
+n = 50
+
+def binary_g(array):
+    #周期回数と振幅「デフォルトより優先される」
+    n = 5
+    a = 1.0
+    
+    x = np.linspace(0, 2*np.pi*n, width)
+    y = (a*np.pi) * np.sin(x) + (a*np.pi)
+    y_binary = np.where(y <= a*np.pi, 0, a*np.pi)
+    
+    plt.plot(y_binary)
+    plt.show()
+    
+    return y_binary
+
 if __name__ == '__main__':
     
     #1. 2次元のグレーティングを設計する
+    g_phase = np.zeros((width, height), dtype = float)
+    binary_g = binary_g(g_phase)
     
     #2. 設計したグレーティングを画像として表示して保存
     
